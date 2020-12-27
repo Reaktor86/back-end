498e4229f994389ab6ff14bdc227a003f9b7310a
Reaktor86 <mindlock861@gmail.com>
2020-12-28 00:03:54 +0300

    123

--- dz_php1.php
+++ dz_php1.php
@@ -1,3 +1,11 @@
+<style>
+    /*для задания №5*/
+    .red {
+        color: red;
+        font-weight: bold;
+    }
+</style>
+
 <?php
 // 1. Вывести имя, компанию, должность
 
@@ -56,7 +64,6 @@ for ($i = 0; $i < 9; $i++) {
     } while ($new <= $nums[$i] || $new - $nums[$i] > 20);
     $nums[] = $new;
 }
-print_r($nums);
 ?>
 
 <p>
@@ -70,14 +77,76 @@ echo "<br><br>";
 
 // 4. округление числа
 
-// генерируем случайное число с плавающей точкой
+// генерируем 10 случайных чисел с плавающей точкой
+$numsOriginal = [];
 for ($i = 0; $i < 10; $i++) {
     $num = rand(0,100);
     $float = rand(0,99999999);
-    $numsOriginal[] = $num + $float / (10 ** strlen($float));
+    $numsOriginal["num" . $i] = $num + $float / (10 ** strlen($float));
 }
 
-// округляем
-/*for ($i = 0; $i < ; $i++) {
+// округляем до 0, 1 или 2 знаков после запятой
+$numsRounded = [];
+for ($i = 0; $i < count($numsOriginal); $i++) {
+    $round = rand(0, 2);
+    $numsRounded[] = round($numsOriginal["num" . $i], $round);
+}
 
-}*/
+// выводим результат
+foreach ($numsRounded as $k => $val): ?>
+    <p>
+        Число <?=$numsOriginal["num" . $k]?> округлено до <?=$val?>
+    </p>
+<?php endforeach; ?>
+
+<?php
+echo "<br><br>";
+/*
+5. Дано 5 строк по 5 случайных чисел в каждом (от 10 до 100)
+Выделить красным минимальное число в каждом СТОЛБЦЕ, посчитать среднее арифметическое
+ */
+
+// генерируем числа
+$numsArray = [];
+
+for ($i = 0; $i < 5; $i++) {
+    $row = [];
+    for ($k = 0; $k < 5; $k++) {
+        $row[] = rand(10,100);
+    }
+    $numsArray[] = $row;
+}
+
+$minArray = []; // здесь будем собирать минимальные числа, которые найдёт алгоритм
+
+// выводим результат
+foreach ($numsArray as $rowNum => $rowArray): ?>
+    <p>
+        <? foreach ($rowArray as $colNum => $num): ?>
+            <? $min = getMin($numsArray, $colNum) ?>
+            <? if ($min == $num): ?>
+                <span class="red"><?=$num . " "?></span>
+                <?$minArray[] = $num?>
+            <? else: ?>
+                <?=$num . " "?>
+            <? endif; ?>
+        <? endforeach; ?>
+    </p>
+<?php endforeach; ?>
+
+<?php
+
+// функция для определения минимального числа в столбце, $column - номер столбца, начиная с 0
+function getMin($numsArray, $column)
+{
+    $temp = [];
+    foreach ($numsArray as $k => $val) {
+        $temp[] = $val[$column];
+    }
+    return min($temp);
+}
+
+// вычисляем среднее значение минимальных чисел
+$average = array_sum($minArray) / count($minArray);
+?>
+<p>Среднее минимальное значение: <?=$average?></p>
